b1c97b6cebfd6242356f9652544045a3759bb189
xujifa <xujifa@yit.com>
2019-04-28 18:39:31 +0800

    整理

---
+++ src/main/java/guide/code/QuickSort.java
@@ -0,0 +1,32 @@
+package guide.code;
+
+/**
+ * Created by Jeff_xu on 06/03/2019.
+ *
+ * @author Jeff_xu
+ */
+public class QuickSort {
+
+    public int[] quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            // 分组
+            int i = left, j = right, x = arr[i];
+            while (i < j) {
+                while (i < j && arr[j] >= x) { j--; }
+                if (i < j) {
+                    arr[i++] = arr[j];
+                }
+                while (i < j && arr[i] < x) {
+                    i++;
+                }
+                if (i < j) {
+                    arr[j--] = arr[i];
+                }
+            }
+            arr[i] = x;
+            quickSort(arr, left, i - 1);
+            quickSort(arr, i + 1, right);
+        }
+        return arr;
+    }
+}
