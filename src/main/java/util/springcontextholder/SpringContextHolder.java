22fcbb825e9a8feeb977a0008cc6c2018b44de63
xujifa <xujifa@yit.com>
2019-11-25 18:14:46 +0800

    添加

---
+++ src/main/java/util/springcontextholder/SpringContextHolder.java
@@ -0,0 +1,75 @@
+package util.springcontextholder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.Validate;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * Created by Jeff_xu on 2019/11/25.
+ *
+ * @author Jeff_xu
+ */
+public class SpringContextHolder implements ApplicationContextAware, DisposableBean {
+
+    private static ApplicationContext applicationContext = null;
+
+    /**
+     * 实现ApplicationContextAware接口, 注入Context到静态变量中.
+     */
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        SpringContextHolder.applicationContext = applicationContext;
+    }
+
+    /**
+     * 取得存储在静态变量中的ApplicationContext.
+     */
+    public static ApplicationContext getApplicationContext() {
+        assertContextInjected();
+        return applicationContext;
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    public static <T> T getBean(String name) {
+        assertContextInjected();
+        return (T)applicationContext.getBean(name);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Bean, 自动转型为所赋值对象的类型.
+     */
+    public static <T> T getBean(Class<T> requiredType) {
+        assertContextInjected();
+        return applicationContext.getBean(requiredType);
+    }
+
+    /**
+     * 从静态变量applicationContext中取得Beans
+     */
+    public static <T> List<T> getBeans(Class<T> requiredType) {
+        assertContextInjected();
+        return new ArrayList<>(applicationContext.getBeansOfType(requiredType).values());
+    }
+
+    /**
+     * 实现DisposableBean接口, 在Context关闭时清理静态变量.
+     */
+    @Override
+    public void destroy() throws Exception {
+        applicationContext = null;
+    }
+
+    /**
+     * 检查ApplicationContext不为空.
+     */
+    private static void assertContextInjected() {
+        Validate.validState(applicationContext != null,
+            "applicationContext属性未注入, 请在applicationContext.xml中定义SpringContextHolder.");
+    }
+}
