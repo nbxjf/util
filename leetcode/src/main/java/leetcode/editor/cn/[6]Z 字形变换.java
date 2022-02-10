0cead95e57672cb6b542ee7fd8efe9c6d8aa3075
Jeff xu <jeff199504@gmail.com>
2022-02-10 14:14:19 +0800

    [6]Z子型变换

---
+++ leetcode/src/main/java/leetcode/editor/cn/[6]Z 字形变换.java
@@ -0,0 +1,111 @@
+package leetcode.editor.cn;
+
+//将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。 
+//
+// 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下： 
+//
+// 
+//P   A   H   N
+//A P L S I I G
+//Y   I   R 
+//
+// 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。 
+//
+// 请你实现这个将字符串进行指定行数变换的函数： 
+//
+// 
+//string convert(string s, int numRows); 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 3
+//输出："PAHNAPLSIIGYIR"
+// 
+//示例 2：
+//
+// 
+//输入：s = "PAYPALISHIRING", numRows = 4
+//输出："PINALSIGYAHRPI"
+//解释：
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "A", numRows = 1
+//输出："A"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 1000 
+// s 由英文字母（小写和大写）、',' 和 '.' 组成 
+// 1 <= numRows <= 1000 
+// 
+// Related Topics 字符串 👍 1466 👎 0
+
+
+class ZigzagConversion {
+    public static void main(String[] args) {
+        Solution solution = new ZigzagConversion().new Solution();
+        String result = solution.convert("PAYPALISHIRING", 4);
+        System.out.println(result);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String convert(String s, int numRows) {
+            if (s == null || s.length() <= 1) {
+                return s;
+            }
+            if (numRows == 1) {
+                return s;
+            }
+            boolean vertical = true; // 标记当前是否是竖直打印
+            Character[][] arrange = new Character[numRows][s.length()]; // 使用数组记录排列后的数据
+
+            int index = 0;
+            int i = 0;
+            int j = 0;
+            while (index <= s.length() - 1) {
+                arrange[i][j] = s.charAt(index);
+                index++;
+                if (vertical) {
+                    i++;
+                    if (i == numRows - 1) { // 到底转换竖直标识
+                        vertical = false;
+                    }
+                } else {
+                    i--;
+                    j++;
+                    if (i == 0) { // 到底转换竖直标识
+                        vertical = true;
+                    }
+                }
+            }
+
+            StringBuilder result = new StringBuilder("");
+            for (Character[] characters : arrange) {
+                for (int k = 0; k < arrange[0].length; k++) {
+                    if (characters[k] != null) {
+                        result.append(characters[k]);
+                    }
+                }
+            }
+            return result.toString();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+
+}
