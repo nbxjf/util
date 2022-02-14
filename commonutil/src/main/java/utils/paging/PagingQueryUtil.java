fe46c9915103dac25e04ae559ce2e3c79611ce1a
Jeff xu <jeff199504@gmail.com>
2022-02-14 16:09:33 +0800

    修改paging query查询插件

--- commonutil/src/main/java/utils/paging/PagingQueryUtil.java
+++ commonutil/src/main/java/utils/paging/PagingQueryUtil.java
@@ -13,17 +13,23 @@ import utils.func.Func1;
 public class PagingQueryUtil {
 
     /**
+     * 默认的分页查询buffer大小
+     */
+    public static final int DEFAULT_BUFFER_SIZE = 5;
+
+    /**
      * 分页查询
      *
      * @param pageParameter 前端的分页参数
      * @param func          func
+     * @param timeoutMillis 超时时间，单位毫秒，如果超过了timeout时间，也会中断分页查询并返回已经查询到的数据
      * @param <T>           T
      * @return result
      */
-    public static <T extends PagingQueryFilterable & Serializable> PagingQueryResp<T> pagingQuery(
-        PageParameter pageParameter,
-        Func1<List<T>, PageParameter> func) {
-        return pagingQuery(5, pageParameter, func);
+    public static <T extends PagingQueryFilterable & Serializable> PagingQueryResp<T> pagingQuery(PageParameter pageParameter,
+                                                                                                  Func1<List<T>, PageParameter> func,
+                                                                                                  long timeoutMillis) {
+        return pagingQuery(DEFAULT_BUFFER_SIZE, pageParameter, func, timeoutMillis);
     }
 
     /**
@@ -32,16 +38,18 @@ public class PagingQueryUtil {
      * @param bufferSize    后端预取数据大小
      * @param pageParameter 前端的分页参数
      * @param func          func
+     * @param timeoutMillis 超时时间，单位毫秒，如果超过了timeout时间，也会中断分页查询并返回已经查询到的数据
      * @param <T>           T
      * @return result
      */
     public static <T extends PagingQueryFilterable & Serializable> PagingQueryResp<T> pagingQuery(int bufferSize,
                                                                                                   PageParameter pageParameter,
-                                                                                                  Func1<List<T>, PageParameter> func) {
+                                                                                                  Func1<List<T>, PageParameter> func,
+                                                                                                  long timeoutMillis) {
         int expectSize = pageParameter.limit;
         pageParameter.limit = pageParameter.limit + bufferSize;
         PagingQueryResp<T> pagingQueryResp = new PagingQueryResp<T>(pageParameter.offset);
-        return pagingQuery(expectSize, pageParameter, func, pagingQueryResp);
+        return pagingQuery(expectSize, pageParameter, func, pagingQueryResp, System.currentTimeMillis(), timeoutMillis);
     }
 
     /**
@@ -51,13 +59,17 @@ public class PagingQueryUtil {
      * @param newPageParameter 当前查询分页参数
      * @param func             func
      * @param pagingQueryResp  返回数据集
+     * @param startTimeMillis  开始执行时间
+     * @param timeoutMillis    超时时间，单位毫秒，如果超过了timeout时间，也会中断分页查询并返回已经查询到的数据
      * @param <T>              T
      * @return result
      */
     private static <T extends PagingQueryFilterable & Serializable> PagingQueryResp<T> pagingQuery(int expectSize,
                                                                                                    PageParameter newPageParameter,
                                                                                                    Func1<List<T>, PageParameter> func,
-                                                                                                   PagingQueryResp<T> pagingQueryResp) {
+                                                                                                   PagingQueryResp<T> pagingQueryResp,
+                                                                                                   long startTimeMillis,
+                                                                                                   long timeoutMillis) {
         List<T> queryResult = func.invoke(newPageParameter);
         for (T t : queryResult) {
             if (expectSize > 0) {
@@ -70,10 +82,14 @@ public class PagingQueryUtil {
                 break;
             }
         }
+        // 如果当前执行时间已经超过了设置的超时时间，跳出递归返回当前的查询结果值，防止死循环
+        if (System.currentTimeMillis() - startTimeMillis >= timeoutMillis) {
+            return pagingQueryResp;
+        }
         // expectSize > 0 ，还需要请求下一页来填充当前的缺失数据
         if (queryResult.size() >= newPageParameter.limit && expectSize > 0) {
             newPageParameter.offset = newPageParameter.offset + newPageParameter.limit;
-            pagingQuery(expectSize, newPageParameter, func, pagingQueryResp);
+            pagingQuery(expectSize, newPageParameter, func, pagingQueryResp, startTimeMillis, timeoutMillis);
         }
         return pagingQueryResp;
     }
